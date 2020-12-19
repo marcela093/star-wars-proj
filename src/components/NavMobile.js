89f443630cdf83a2f620fd2d509778290a67ce52
Marcela <marcela.souza093@gmail.com>
2020-12-18 22:37:19 -0300

    feat: add skeleton and new configs

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -1,40 +1,30 @@
-import React, { useState } from "react";
-import Button from "@material-ui/core/Button";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
-import { Link } from "react-router-dom";
-import "../styles/navMobile.css";
-import MenuIcon from "@material-ui/icons/Menu";
+import React, { useState } from 'react'
+import Button from '@material-ui/core/Button'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import { Link } from 'react-router-dom'
+import '../styles/navMobile.css'
+import MenuIcon from '@material-ui/icons/Menu'
 
 const NavMobile = () => {
-  const [anchorEl, setAnchorEl] = useState(null);
+  const [anchorEl, setAnchorEl] = useState(null)
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget)
+  }
 
   const handleClose = () => {
-    setAnchorEl(null);
-  };
+    setAnchorEl(null)
+  }
 
   return (
     <div className="nav-mobile__container">
-      <Button
-        aria-controls="simple-menu"
-        aria-haspopup="true"
-        onClick={handleClick}
-      >
+      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
         <div className="menu">
           <MenuIcon />
         </div>
       </Button>
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
+      <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
         <MenuItem onClick={handleClose}>
           <Link to="/characters" className="nav-mobile__item">
             Characters
@@ -57,7 +47,7 @@ const NavMobile = () => {
         </MenuItem>
       </Menu>
     </div>
-  );
-};
+  )
+}
 
-export default NavMobile;
+export default NavMobile
--- src/pages/Films.js
+++ src/pages/Films.js
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from "react";
-import "../styles/films.css";
-import IV from "../assets/IV.jpg";
-import V from "../assets/V.jpg";
-import VI from "../assets/VI.jpg";
-import I from "../assets/I.jpg";
-import II from "../assets/II.jpg";
-import III from "../assets/III.jpg";
-import Nav from "../components/Nav";
+import React, { useEffect, useState } from 'react'
+import { Skeleton } from '@material-ui/lab'
+
+import '../styles/films.css'
+
+import IV from '../assets/IV.jpg'
+import V from '../assets/V.jpg'
+import VI from '../assets/VI.jpg'
+import I from '../assets/I.jpg'
+import II from '../assets/II.jpg'
+import III from '../assets/III.jpg'
+
+import Nav from '../components/Nav'
 
 const moviesImages = [
   { id: 4, img: IV },
@@ -15,45 +19,48 @@ const moviesImages = [
   { id: 1, img: I },
   { id: 2, img: II },
   { id: 3, img: III },
-];
+]
 
 const Films = () => {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    fetch("https://swapi.dev/api/films/")
+    setLoading(true)
+    fetch('https://swapi.dev/api/films/')
       .then((res) => res.json())
       .then(
         (data) => {
-          setMovies(data.results);
+          setMovies(data.results)
+          setLoading(false)
         },
         (error) => {
-          return console.log("deu ruim", error);
+          return console.log('deu ruim', error)
         }
-      );
-  }, []);
+      )
+  }, [])
 
   const getImages = () => {
     movies.forEach((movie) => {
       moviesImages.forEach((image) => {
         if (movie.episode_id === image.id) {
-          movie.image = image.img;
-          movie.image_id = image.id;
+          movie.image = image.img
+          movie.image_id = image.id
         }
-      });
-    });
-  };
+      })
+    })
+  }
 
-  getImages();
+  getImages()
 
   return (
     <div>
       <Nav />
       <div className="films__container">
         <div className="films">
-          {movies.map((movie) => {
-            return (
-              <div className="films__card">
+          {(loading ? Array.from(new Array(6)) : movies).map((movie) => (
+            <div className="films__card">
+              {movie ? (
                 <div className="films__image-box">
                   <img
                     src={movie.image}
@@ -61,19 +68,50 @@ const Films = () => {
                     alt="star wars poster movie"
                   />
                 </div>
-
+              ) : (
+                <Skeleton
+                  className="films__image"
+                  variant="rect"
+                  style={{
+                    backgroundColor: '#2d2d2d',
+                    height: '40vh',
+                    width: '70%',
+                    marginTop: '2vh',
+                  }}
+                />
+              )}
+              {movie ? (
                 <div className="films__info">
                   <h4 className="films__title">{movie.title}</h4>
                   <p>{`Release Date: ${movie.release_date}`}</p>
                   <p>{movie.opening_crawl}</p>
                 </div>
-              </div>
-            );
-          })}
+              ) : (
+                <>
+                  <Skeleton
+                    height={40}
+                    className="films__info"
+                    style={{ backgroundColor: '#2d2d2d', width: '90%' }}
+                  />
+
+                  <Skeleton
+                    className="films__info"
+                    variant="rect"
+                    height={200}
+                    style={{
+                      backgroundColor: '#2d2d2d',
+                      marginBottom: '2vh',
+                      width: '90%',
+                    }}
+                  />
+                </>
+              )}
+            </div>
+          ))}
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Films;
+export default Films
